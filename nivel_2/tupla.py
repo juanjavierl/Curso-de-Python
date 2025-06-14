0dbb8a7e4c5c96c9ba57d6a7935039f2ba3d7923
jjavierl <jjavierl.bo@gmail.com>
2025-06-13 21:14:15 -0400

    manejo de tupla en python

--- nivel_2/tupla.py
+++ nivel_2/tupla.py
@@ -1,3 +1,33 @@
-t = (46, 35, 22, 61, 40.5, "Maria", True, (1, 2, 3))
+tupla = (46, 35, 22, 61, 40.5, "Maria", True, (1, 2, 3))
+tupla[0]
+valor = 22
+if valor in tupla:
+    print("Valor encontrado")
+else:
+    print("Valor no existe")
 
-print(t[5].upper())
+print(len(tupla)) 
+
+""" mi_tupla = (10, 20, 30, 40)
+
+for valor in mi_tupla:
+    print(valor) """
+
+""" for indice in range(0,len(mi_tupla)):
+    print(mi_tupla[indice] + 5) """
+
+
+
+
+
+#datos_estudiente()
+
+def verificar_orden(tupla):
+    for i in range(len(tupla) -1):
+        if tupla[i] > tupla[i+1]:
+            return False
+    return True
+
+
+mi_tupla = (1,2,3,4,5,6)
+print(verificar_orden(mi_tupla))
